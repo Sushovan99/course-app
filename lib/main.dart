4c63ad032a5b51e022f35b1862c732ee32f440eb
Sushovan Biswas <biswassushovan99@gmail.com>
2022-12-07 19:14:08 +0530

    Add Fade animation for sidebar backdrop

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'package:course_app/components/list/recent_course_list.dart';
-import 'package:course_app/constants.dart';
-import 'package:course_app/screens/sidebar_screen.dart';
 import 'package:flutter/material.dart';
-import 'components/home_screen_nav.dart';
-import 'components/list/explore_course_list.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(const App());
@@ -17,108 +13,6 @@ class App extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
-  late Animation<Offset> sidebarAnimation;
-  late AnimationController sidebarAnimationController;
-
-  @override
-  void initState() {
-    super.initState();
-    sidebarAnimationController = AnimationController(
-      vsync: this,
-      duration: const Duration(
-        milliseconds: 250,
-      ),
-    );
-    sidebarAnimation = Tween<Offset>(
-      begin: const Offset(-1, 0),
-      end: const Offset(0, 0),
-    ).animate(
-      CurvedAnimation(
-        parent: sidebarAnimationController,
-        curve: Curves.easeInOut,
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          SafeArea(
-            child: Container(
-              color: kBackgroundColor,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  HomeScreenNavbar(
-                    triggerAnimation: () {
-                      sidebarAnimationController.forward();
-                    },
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text(
-                          'Recents',
-                          style: kLargeTitleStyle,
-                        ),
-                        const SizedBox(
-                          height: 5.0,
-                        ),
-                        Text(
-                          '23 courses, more coming',
-                          style: kSubtitleStyle,
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 28.0,
-                  ),
-                  const RecentCourseList(),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      top: 25.0,
-                      left: 20.0,
-                      right: 20.0,
-                      bottom: 16.0,
-                    ),
-                    child: Text(
-                      'Explore',
-                      style: kTitle1Style,
-                    ),
-                  ),
-                  const ExploreCourseList(),
-                ],
-              ),
-            ),
-          ),
-          SlideTransition(
-            position: sidebarAnimation,
-            child: const SafeArea(
-              bottom: false,
-              child: SidebarScreen(),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+
+import '../components/home_screen_nav.dart';
+import '../components/list/explore_course_list.dart';
+import '../components/list/recent_course_list.dart';
+import '../constants.dart';
+import 'sidebar_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  late Animation<Offset> sidebarAnimation;
+  late AnimationController sidebarAnimationController;
+  late Animation<double> fadeAnimation;
+  var sidebarHidden = false;
+
+  @override
+  void initState() {
+    super.initState();
+    sidebarAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 250,
+      ),
+    );
+
+    sidebarAnimation = Tween<Offset>(
+      begin: const Offset(-1, 0),
+      end: const Offset(0, 0),
+    ).animate(
+      CurvedAnimation(
+        parent: sidebarAnimationController,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    fadeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1,
+    ).animate(
+      CurvedAnimation(
+        parent: sidebarAnimationController,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    sidebarAnimationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SafeArea(
+            child: Container(
+              color: kBackgroundColor,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  HomeScreenNavbar(
+                    triggerAnimation: () {
+                      setState(() {
+                        sidebarHidden = !sidebarHidden;
+                      });
+                      sidebarAnimationController.forward();
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          'Recents',
+                          style: kLargeTitleStyle,
+                        ),
+                        const SizedBox(
+                          height: 5.0,
+                        ),
+                        Text(
+                          '23 courses, more coming',
+                          style: kSubtitleStyle,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 28.0,
+                  ),
+                  const RecentCourseList(),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 25.0,
+                      left: 20.0,
+                      right: 20.0,
+                      bottom: 16.0,
+                    ),
+                    child: Text(
+                      'Explore',
+                      style: kTitle1Style,
+                    ),
+                  ),
+                  const ExploreCourseList(),
+                ],
+              ),
+            ),
+          ),
+          IgnorePointer(
+            ignoring: sidebarHidden,
+            child: Stack(children: [
+              FadeTransition(
+                opacity: fadeAnimation,
+                child: GestureDetector(
+                  child: Container(
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: const BoxDecoration(
+                      color: Color.fromRGBO(36, 38, 41, 0.4),
+                    ),
+                  ),
+                  onTap: () {
+                    setState(() {
+                      sidebarHidden = !sidebarHidden;
+                    });
+                    sidebarAnimationController.reverse();
+                  },
+                ),
+              ),
+              SlideTransition(
+                position: sidebarAnimation,
+                child: const SafeArea(
+                  bottom: false,
+                  child: SidebarScreen(),
+                ),
+              ),
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+}
